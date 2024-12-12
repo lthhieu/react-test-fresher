b0f19d282a6575ac44cf966faeecc370547452a7
Hieu ward 3 <lyhieu2024@gmail.com>
2024-12-12 15:24:23 +0700

    absolute path in vite

--- fe-ts/src/main.tsx
+++ fe-ts/src/main.tsx
@@ -1,20 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@/index.css'
 import { BrowserRouter, Routes, Route } from "react-router";
+import App from '@/App';
+import BookPage from '@/pages/client/book';
+import HomePage from '@/pages/client/home';
+import AboutPage from '@/pages/client/about';
+import LoginPage from '@/pages/client/auth/login';
+import RegisterPage from '@/pages/client/auth/register';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} >
-          <Route index element={<div>home</div>} />
-          <Route path="books" element={<div>books</div>} />
-          <Route path="about" element={<div>about</div>} />
+          <Route index element={<HomePage />} />
+          <Route path="books" element={<BookPage />} />
+          <Route path="about" element={<AboutPage />} />
         </Route>
-        <Route path="/login" element={<div>login</div>} />
-        <Route path="/register" element={<div>register</div>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
