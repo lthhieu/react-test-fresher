d92398b661a98a88a8f71b5e6e8d63f9fed519e3
Hieu ward 3 <lyhieu2024@gmail.com>
2024-12-13 16:40:24 +0700

    design register page

--- fe-ts/src/pages/client/auth/register.tsx
+++ fe-ts/src/pages/client/auth/register.tsx
@@ -1,6 +1,87 @@
+import type { FormProps } from 'antd';
+import { Button, Card, Flex, Form, Input, Divider } from 'antd';
+import { useState } from 'react';
+
+type FieldType = {
+    fullName?: string;
+    email?: string;
+    password?: string;
+    phone?: string;
+};
+
+const boxStyle: React.CSSProperties = {
+    width: '100%',
+    height: '100vh',
+    backgroundColor: '#f0f2f5'
+};
 const RegisterPage = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        setLoading(true)
+        setTimeout(() => { setLoading(false); console.log(values) }, 3000)
+    };
+
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
-        <div>RegisterPage</div>
+        <Flex style={boxStyle} justify='center' align='center'>
+            <Card title={<span style={{ fontSize: '24px', fontWeight: 700, justifyContent: 'center', width: '100%', display: 'flex' }}>Đăng ký</span>} style={{ minWidth: 500 }}>
+                <Form
+                    layout='vertical'
+                    name="register"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+
+                >
+                    <Form.Item<FieldType>
+                        label="Họ tên"
+                        name="fullName"
+                        rules={[{ required: true, message: 'Please input your fullName!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item<FieldType>
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email!' },
+                        { type: 'email', message: 'This is not a valid email!' }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item<FieldType>
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item<FieldType>
+                        label="Số điện thoại"
+                        name="phone"
+                        rules={[{ required: true, message: 'Please input your phone!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item label={null}
+                    >
+                        <Button block type="primary" htmlType="submit" loading={loading}>
+                            Đăng ký
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <Divider plain>Hoặc</Divider>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <a href="#">Đăng nhập bây giờ!</a>
+                </div>
+            </Card>
+        </Flex>
     )
 }
 export default RegisterPage
