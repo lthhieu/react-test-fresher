f767fa4e76ff74b787007ae5de748a4f921da06d
Hieu ward 3 <lyhieu2024@gmail.com>
2024-12-17 10:49:38 +0700

    spin antd

--- fe-ts/src/App.tsx
+++ fe-ts/src/App.tsx
@@ -1,20 +1,31 @@
 import { Outlet } from "react-router"
 import AppHeader from "./components/layout/app.header"
 import { useEffect } from "react"
-import { useAppDispatch } from "@/redux/hooks"
-import { handleFetchAccountAsync } from "@/redux/feature/account/accountSlice"
+import { useAppDispatch, useAppSelector } from "@/redux/hooks"
+import { handleFetchAccountAsync, selectLoading } from "@/redux/feature/account/accountSlice"
+import { Spin } from "antd"
 
+const centerStyle: React.CSSProperties = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  marginTop: '-50px',
+  marginLeft: '-50px'
+};
 function App() {
   const dispatch = useAppDispatch()
+  const loading = useAppSelector(selectLoading)
   useEffect(() => {
     dispatch(handleFetchAccountAsync())
   }, [])
 
   return (
-    <div>
-      <AppHeader />
-      <Outlet />
-    </div>
+    <>
+      {loading === 'loading' ? <Spin style={centerStyle} size="large" /> : <>
+        <AppHeader />
+        <Outlet />
+      </>}
+    </>
   )
 }
 
