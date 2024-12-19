0198239d338c24e898e6f1a5fe217b2c8ed9c7b6
Hieu ward 3 <lyhieu2024@gmail.com>
2024-12-19 17:11:09 +0700

    base layout client

--- fe-ts/src/pages/layout/layout.client.tsx
+++ fe-ts/src/pages/layout/layout.client.tsx
@@ -1,11 +1,64 @@
 
 import { Outlet } from "react-router"
+import React from 'react';
+import { Flex, Layout } from 'antd';
+import { AndroidOutlined } from "@ant-design/icons";
+
+const { Header, Footer, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    // height: 64,
+    paddingInline: 100,
+    lineHeight: '64px',
+    backgroundColor: '#4096ff',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+};
+
+const contentStyle: React.CSSProperties = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: 'red',
+    backgroundColor: '#fff',
+};
+
+const footerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fffc',
+    backgroundColor: '#001529',
+};
+
+const layoutStyle: React.CSSProperties = {
+    // borderRadius: 8,
+    overflow: 'hidden',
+    width: '100%',
+    maxWidth: '100%',
+    minHeight: '100vh'
+};
 
 const LayoutClient = () => {
     return (
         <>
-            <div>layout client</div>
-            <Outlet />
+            <Layout style={layoutStyle}>
+                <Header style={headerStyle}>
+                    <div style={{ display: 'flex' }}>
+                        <AndroidOutlined style={{ fontSize: '32px', color: '#fffc' }} />
+                        <p>MenuFoldOutlined</p>
+                    </div>
+                    <div style={{ width: '500px' }}>
+                        <p>MenuFoldOutlined</p>
+
+                    </div>
+                    <div>ava cart</div>
+                </Header>
+                <Content style={contentStyle}><Outlet /></Content>
+                <Footer style={footerStyle}>Footer</Footer>
+            </Layout >
+
         </>
     )
 }
